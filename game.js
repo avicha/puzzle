5012fa60926251b620941383ccb53419268bc862
AV <av@admindeMacBook-Pro.local>
2018-03-06 23:36:13 +0800

    refactor:重构了结果页面和增加了成绩记录，精简了关卡的设计，修改了部分窗花素材

--- game.js
+++ game.js
@@ -21,7 +21,7 @@ import Adapter from 'app/adapter'
 let game = new Engine({ debug: false, stageScaleMode: 'cover', fps: 60, orientation: 'landscape' })
 game.setStageSize(1780, 750)
 Adapter.setStorage('scene1-1_active', true)
-game.launch(StageSelector3)
+game.launch(Menu)
 game.on('switchScene', (sceneName, ...args) => {
     game.pause()
     game._context.fillStyle = '#383838'
